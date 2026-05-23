00e407afe219c8526e61b4ec072df15aaa56b926
Giwon Kim <gohouse1221@gmail.com>
2026-05-23 16:54:36 +0900

    feat: 스케줄러 활성화 @EnableScheduling 추가

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -4,9 +4,11 @@ import com.example.backend.file.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
+@EnableScheduling
 public class BackendApplication {
 
   public static void main(String[] args) {
